83b96e0da507be42afc701e1f86dbe725394ceb0
karanu-171 <karanujohn171@gmail.com>
2023-07-14 11:05:20 +0300

    final commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB17f387l-eIQ1El5rODZT5-SN9-ytYPX4",
-  authDomain: "todo-app-bdfa4.firebaseapp.com",
-  projectId: "todo-app-bdfa4",
-  storageBucket: "todo-app-bdfa4.appspot.com",
-  messagingSenderId: "443704624233",
-  appId: "1:443704624233:web:89a9a12ea86dbe94a00ef8"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 // Initialize Firebase
